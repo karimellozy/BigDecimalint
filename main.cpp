a484f66e6fd69eb7358d4bb8391be6c2131a4fee
karemhasanen <102561068+karemhasanen@users.noreply.github.com>
2022-10-25 14:28:39 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include "BigDecimalInt.h"
+
+int main() {
+//    BigDecimalInt num1("99999999999999999999999999999999999999999988888888888888888888888888888888888888888888888888");
+//    BigDecimalInt num8;
+//    num8 = num1;
+//    cout << num8;
+//    BigDecimalInt num6("-123456789012345678901234567890");
+
+//    BigDecimalInt num2("777777777777777777777777777777777777777777666666666666666666666666666666666");
+//    cout << num1 + num2;
+//    if (num2 > num1){
+//        cout << "hjk\n";
+//    }
+//    10,000,000,001,000,000,000,100,000,000
+//cout << num1 - num2;
+//    278,243,784,461,124,220,486,190,008,392,609,455,714,328,697,380,261
+
+
+//    BigDecimalInt num("19996432458619264");
+//    BigDecimalInt num_("6435217894206149");
+//    cout << num - num_;
+//    cout << num2;
+//    BigDecimalInt num3("-200000000000000000000000000000");
+//    BigDecimalInt num7("-100000000000000000000000000000");
+//    bool b = num7 > num3;
+//    cout << b;
+//    bool k = num3 == num7;
+//    cout << k;
+//    BigDecimalInt num4;
+//            cout << num2 - num1;
+
+
+
+//    BigDecimalInt num1("123456789012345678901234567890");
+//    BigDecimalInt num2("+113456789011345678901134567890754612301545");
+//    BigDecimalInt num3("-200000000000000000000000000000");
+//    BigDecimalInt num4 = num2 + num1;
+//    BigDecimalInt num5 = num2 - num1;
+//    cout << "num1 = " << num1 << endl;
+//    cout << "num2 = " << num2 << endl;
+//Next statement will print 236913578023691357802369135780
+//    cout << "num2 + num1 = " << num4 << endl;
+//Next statement will print -313456789011345678901134567890
+//    cout << "num2 - num1 = " << num5 << endl;
+
+    BigDecimalInt num("999666555444333222111");
+    BigDecimalInt num_("9876543210");
+    cout << num + num_;
+    return 0;
+}
